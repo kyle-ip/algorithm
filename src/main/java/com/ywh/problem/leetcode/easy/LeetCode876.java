6d58fac7d87cbd2e5acb9f4cb3e8694b6b48b8f2
yeyongkang <yeyongkang@xdh.net.cn>
2019-11-04 12:46:16 +0800

    :rocket: New Problems' Solutions 448(easy), 543(easy), 876(easy)

--- src/main/java/com/ywh/problem/leetcode/easy/LeetCode876.java
+++ src/main/java/com/ywh/problem/leetcode/easy/LeetCode876.java
@@ -13,35 +13,33 @@ public class LeetCode876 {
 
     /**
      * 两次遍历，第一次计算出链表长度，第二次移动到中间位置即返回
+     * Time: O(n), Space: O(1)
      *
      * @param head
      * @return
      */
     public ListNode getMiddleNodeTwoPass(ListNode head) {
         int len = 0;
-        for (ListNode cur = head; cur != null; len += 1, cur = cur.next) ;
-
+        for (ListNode cur = head; cur != null; len++, cur = cur.next);
         ListNode cur = head;
-        for (int i = 0; i < len / 2; cur = cur.next, i++) ;
+        for (int i = 0; i < len / 2; cur = cur.next, i++);
         return cur;
-
     }
 
     /**
      * 一次遍历，使用两个指针，快指针每次走两步，慢指针每次走一步；
      * 当快指针走到尽头即返回慢指针的节点
+     * Time: O(n), Space: O(1)
      *
      * @param head
      * @return
      */
     public ListNode getMiddleNodeOnePass(ListNode head) {
         ListNode fast = head, slow = head;
-
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
-
         return slow;
     }
 }
