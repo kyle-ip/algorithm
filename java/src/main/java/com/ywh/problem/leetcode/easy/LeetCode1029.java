c1559cbc7a63d271b2e3c1ec8121553c4b4669a7
yipwinghong <yipwinghong@outlook.com>
2019-12-03 22:06:00 +0800

    :rocket: New Problems' Solutions 1029(easy)

---
+++ java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode1029.java
@@ -0,0 +1,41 @@
+package com.ywh.problem.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 两地调度
+ * [贪心]
+ *
+ * @author ywh
+ * @since 03/12/2019
+ */
+public class LeetCode1029 {
+
+    /**
+     * Time: O(n*log(n)), Space: O(n)
+     *
+     * @param costs
+     * @return
+     */
+    public static int twoCitySchedCost(int[][] costs) {
+
+        // 先假设所有人都去 A 城，把数组根据每个人去 A 城和去 B 城的成本之差排序（排在前面的表示去 A 城成本较低、排在后面的表示去 B 城成本较低）
+        Arrays.sort(costs, Comparator.comparingInt(o -> (o[0] - o[1])));
+        int sum = 0, n = costs.length / 2;
+
+        // 前一半选择去 A 城、后一半选择去 B 城
+        // 比如 [[10,20],[30,200],[400,50],[30,20]]，对于第一个差值为 -10（较小，会排在前面），表示去 A 城的成本比去 B 城高 -10，因此选去 A 城
+        for (int i = 0; i < n; ++i) {
+            sum += costs[i][0] + costs[i + n][1];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] input = new int[][]{{10, 20}, {30, 200}, {400, 50}, {30, 20}};
+        System.out.println(twoCitySchedCost(input));
+    }
+
+}
+
