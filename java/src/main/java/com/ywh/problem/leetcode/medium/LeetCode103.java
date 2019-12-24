a8bf2f0c410b05e5b781948e0012f1eda272c8e8
yipwinghong <yipwinghong@outlook.com>
2019-12-24 20:04:59 +0800

    :rocket: New Problems' Solutions 103(medium)

---
+++ java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode103.java
@@ -0,0 +1,89 @@
+package com.ywh.problem.leetcode.medium;
+
+import com.ywh.model.TreeNode;
+
+import java.util.*;
+
+/**
+ * 二叉树的 Z 字形遍历
+ * [树] [BFS] [队列]
+ *
+ * @author ywh
+ * @since 24/12/2019
+ */
+public class LeetCode103 {
+
+
+    /**
+     * Time: O(n), Space: O(n)
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean right2Left = false;
+        while (!q.isEmpty()) {
+            List<Integer> elem = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                elem.add(node.val);
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            if (right2Left) {
+                Collections.reverse(elem);
+            }
+            result.add(elem);
+            right2Left = !right2Left;
+        }
+        return result;
+    }
+
+    /**
+     * Time: O(n), Space: O(n)
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> zigzagLevelOrderV2(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean right2Left = false;
+        while (!q.isEmpty()) {
+            List<Integer> elem = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (right2Left) {
+                    elem.add(0, node.val);
+                } else {
+                    elem.add(node.val);
+                }
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            result.add(elem);
+            right2Left = !right2Left;
+        }
+        return result;
+    }
+}
