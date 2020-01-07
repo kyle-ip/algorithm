b3fba24cbc00a20d27ddbe0d6d5f22c56b05b863
yipwinghong <yipwinghong@outlook.com>
2020-01-07 22:41:29 +0800

    :rocket: New Problems' Solutions 188(medium)

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Data Structure &amp; Algorithm Problems' Solutions for interview, implemented in
 
 
 
-## LeetCode - Algorithm (187)
+## LeetCode - Algorithm (188)
 
 > Solutions implemented in C, Python coming soon.
 
@@ -152,6 +152,7 @@ Data Structure &amp; Algorithm Problems' Solutions for interview, implemented in
 | 238  | [Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self) | 除了自身元素的数组乘积             | [Java](java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode238.java) | Medium     | [Array]                                                  |
 | 239  | [Sliding Window Maximum](https://leetcode.com/problems/sliding-window-maximum) | 滑动窗口中的最大值                 | [Java](java/src/main/java/com/ywh/problem/leetcode/hard/LeetCode239.java) | Hard       | [Heap]                                                   |
 | 240  | [Search a 2D Matrix II](https://leetcode.com/problems/search-a-2d-matrix-ii/) | 行列递增的二维数组搜索             | [Java](java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode240.java) | Medium     | [Binary Search] [Divide and Conquer]                     |
+| 255  | [Verify Preorder Sequence in Binary Search Tree](https://leetcode.com/problems/verify-preorder-sequence-in-binary-search-tree) | 验证二叉搜索树的前序遍历序列       | [Java](java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode255.java) | Medium     | [Tree] [Stack] [Divide and Conquer]                      |
 | 263  | [Ugly Number](https://leetcode.com/problems/ugly-number/)    | 丑数                               | [Java](java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode263.java) | Easy       | [Math]                                                   |
 | 268  | [Missing Number](https://leetcode.com/problems/missing-number/) | 缺失的数字                         | [Java](java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode268.java) | Medium     | [Array] [Math] [Bit Manipulation]                        |
 | 283  | [Move Zeroes](https://leetcode.com/problems/move-zeroes/)    | 向后移动 0                         | [Java](java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode283.java) | Easy       | [Array] [Two Pointers]                                   |
---
+++ java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode255.java
@@ -0,0 +1,146 @@
+package com.ywh.problem.leetcode.medium;
+
+import java.util.Stack;
+
+/**
+ * 验证二叉搜索树的前序遍历序列
+ * [树] [栈] [分治]
+ *
+ * @author ywh
+ * @since 07/01/2020
+ */
+public class LeetCode255 {
+
+    /**
+     * 每次处理 [start, end) 范围内的值，验证是否前序遍历
+     *
+     * @param nums
+     * @param start
+     * @param end
+     * @return
+     */
+    private boolean verify(int[] nums, int start, int end) {
+        if (start == end || start + 1 == end) {
+            return true;
+        }
+        // 取树根：[4], 1, 0, 2, 8
+        int root = nums[start];
+
+        // 验证左子树：4, (1, 0, 2), 8
+        int i = start + 1;
+        while (i < end && nums[i] < root) {
+            ++i;
+        }
+
+        // 验证右子树：4, 1, 0, 2, (8)
+        int mid = i;
+        while (i < end && nums[i] > root) {
+            ++i;
+        }
+
+        // 如果验证到尽头（满足根节点大于左子树、小于右子树的条件），则递归验证其左右子树，否则返回 false
+        if (i != end) {
+            return false;
+
+        }
+        return verify(nums, start + 1, mid) && verify(nums, mid, end);
+    }
+
+    /**
+     * 分治解法（自顶向下）
+     *
+     * Time: O(n^2), Space: O(n)
+     *
+     * @param preorder
+     * @return
+     */
+    public boolean verifyPreorderDivideConquer(int[] preorder) {
+        if (preorder == null) {
+            return false;
+        }
+        return verify(preorder, 0, preorder.length);
+    }
+
+    /**
+     * 栈解法（自底向上，每轮对比结果可以复用下一轮）
+     *
+     * Time: O(n), Space: O(n)
+     *
+     * @param preorder
+     * @return
+     */
+    public boolean verifyPreorderStack(int[] preorder) {
+        if (preorder == null) return false;
+
+        Stack<Integer> stack = new Stack<>();
+        int lowerBound = Integer.MIN_VALUE;
+
+        // 逐个元素判断
+        for (int num: preorder) {
+
+            // 如果元素小于下界，表示右子树的元素小于父节点或左子树
+            if (num < lowerBound) {
+                return false;
+            }
+            // 如果栈非空，且当前元素大于栈顶元素，则求当前元素在栈中的下界（下界表示栈中小于当前元素的最大值）
+            while (!stack.isEmpty() && num > stack.peek()) {
+                lowerBound = stack.pop();
+            }
+
+            // 逐个元素入栈（当出现当前元素比栈顶元素大时会不断出栈、求下界，因此相当于先把左分支入栈、再处理右分支）
+            stack.push(num);
+        }
+        return true;
+    }
+
+    /**
+     * 运用数组实现栈
+     *
+     * Time: O(n), Space: O(1)
+     *
+     * @param preorder
+     * @return
+     */
+    public boolean verifyPreorderArray(int[] preorder) {
+        if (preorder == null) {
+            return false;
+        }
+        int[] stack = new int[preorder.length];
+        int lowerBound = Integer.MIN_VALUE, top = -1;
+        for (int num : preorder) {
+            if (num < lowerBound) {
+                return false;
+            }
+            while (top != -1 && num > stack[top]) {
+                lowerBound = stack[top--];
+            }
+            stack[++top] = num;
+        }
+        return true;
+    }
+
+    /**
+     * 复用入参数组
+     *
+     * Time: O(n), Space: O(1)
+     *
+     * @param preorder
+     * @return
+     */
+    public boolean verifyPreorderArrayO1Space(int[] preorder) {
+        if (preorder == null) {
+            return false;
+        }
+        int lowerBound = Integer.MIN_VALUE, top = -1;
+        for (int num : preorder) {
+            if (num < lowerBound) {
+                return false;
+            }
+            while (top != -1 && num > preorder[top]) {
+                lowerBound = preorder[top--];
+            }
+            preorder[++top] = num;
+        }
+        return true;
+    }
+}
