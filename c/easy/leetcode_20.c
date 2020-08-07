d8ca6e55a452844ef1ec6e1f9a538f42dc132b64
yipwinghong <yipwinghong@outlook.com>
2020-08-07 14:36:58 +0800

    :tada: New Problem's Solutions in C (20 easy)

---
+++ c/easy/leetcode_20.c
@@ -0,0 +1,30 @@
+//
+// Created by ywh on 2020/8/7.
+//
+
+bool isValid(char * s){
+    if (s == NULL || strlen(s) == 0) {
+        return 1;
+    }
+    char stack[strlen(s)];
+    int n = 0;
+    while (*s) {
+        switch (*s) {
+            case '(':
+                stack[n++] = ')';
+                break;
+            case '[':
+                stack[n++] = ']';
+                break;
+            case '{':
+                stack[n++] = '}';
+                break;
+            default:
+                if (n == 0 || *s != stack[--n]) {
+                    return 0;
+                }
+        }
+        s++;
+    }
+    return n == 0;
+}
