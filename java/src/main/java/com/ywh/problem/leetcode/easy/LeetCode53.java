3007e9c9f1662de9920daef84c4531e312d1d76c
yipwinghong <yipwinghong@outlook.com>
2020-10-03 20:25:11 +0800

    :pencil: Update README.md (code review)

--- java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode53.java
+++ java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode53.java
@@ -10,24 +10,24 @@ package com.ywh.problem.leetcode.easy;
 public class LeetCode53 {
 
     /**
-     * 遍历数组，使用变量 cur 记录当前累计和、max 记录历史最大累计和；
-     * 当 cur > 0，表示当前累计和仍存在成为全局最大的可能性，所以继续累计数组中的元素；
-     * 当 cur <= 0，表示无论下一个元素是正是负，都没有必要把当前累计和加上，因此抛弃当前累计和、从当前元素开始累计；
-     * 每轮计算都把 cur 与 max 比较，取较大者保存在 max 可确保 max 始终保持最大的累计和
-     *
      * Time: O(n), Space: O(1)
      *
      * @param nums
      * @return
      */
     public int maxSumOfSubArray(int[] nums) {
+        // 遍历数组，使用变量 cur 记录当前累计和、max 记录历史最大累计和。
         int max = Integer.MIN_VALUE, cur = 0;
         for (int num : nums) {
+            // 当 cur > 0，表示当前累计和仍存在成为全局最大的可能性，所以继续累计数组中的元素。
             if (cur > 0) {
                 cur += num;
-            } else {
+            }
+            // 当 cur <= 0，表示无论下一个元素是正是负，都没有必要把当前累计和加上（因为会使总和变小），因此抛弃当前累计和、从当前元素重新开始累计。
+            else {
                 cur = num;
             }
+            // 每轮计算都把 cur 与 max 比较，取较大者保存在 max 可确保 max 始终保持最大的累计和。
             max = Math.max(cur, max);
         }
         return max;
--- java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode15.java
+++ java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode15.java
@@ -50,13 +50,43 @@ public class LeetCode15 {
      * @return
      */
     public List<List<Integer>> threeNumSumToZeroOn2(int[] nums) {
+
         List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[j] == 0) {
+                        List<Integer> elem = Arrays.asList(nums[i], nums[j], nums[k]);
+                        if (set.contains(elem)) {
+                            set.add(elem);
+                            result.add(elem);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 排序 + 双指针（注意每次成功判断后都要跳过重复值）
+     *
+     * Time: O(n^2), Space: O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeNumSumToZeroOn(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
         Arrays.sort(nums);
         for (int k = nums.length - 1; k >= 2; k--) {
+            // 如果 nums[k] 已经小于 0，则其左边都是负数，三个负数之和不可能为 0。
             if (nums[k] < 0) {
                 break;
             }
-
             int target = -nums[k], left = 0, right = k - 1;
             while (left < right) {
                 if (nums[left] + nums[right] < target) {
@@ -64,9 +94,8 @@ public class LeetCode15 {
                 } else if (nums[left] + nums[right] > target) {
                     right--;
                 } else {
-                    result.add(Arrays.asList(nums[left], nums[right], nums[k]));
-
-                    // 跳过重复值
+                    ret.add(Arrays.asList(nums[left], nums[right], nums[k]));
+                    // 跳过重复值。
                     for (; left < right && nums[left] == nums[left + 1]; left++);
                     for (; left < right && nums[right] == nums[right - 1]; right--);
                     left++;
@@ -74,11 +103,9 @@ public class LeetCode15 {
                 }
             }
 
-            // 跳过重复值
-            while (k >= 2 && nums[k] == nums[k - 1]) {
-                k--;
-            }
+            // 跳过重复值。
+            for (; k>= 2 && nums[k] == nums[k - 1];);
         }
-        return result;
+        return ret;
     }
 }
