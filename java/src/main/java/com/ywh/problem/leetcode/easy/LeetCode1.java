e9f0ad9d2a27e32eb65ebe78a3fa7ebc372bf38c
yipwinghong <yipwinghong@outlook.com>
2020-11-08 11:21:39 +0800

    :pencil: Update comments

--- java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode1.java
+++ java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode1.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class LeetCode1 {
 
     /**
-     * 两层循环，注意内层循环与外层错开 1 位，避免得出相同元素组合
+     * 两层循环，注意内层循环与外层错开 1 位，避免得出相同元素组合。
      *
      * Time: O(n^2), Space: O(1)
      *
@@ -34,11 +34,6 @@ public class LeetCode1 {
     }
 
     /**
-     * 一层循环
-     * 创建一个缓存 Map，每轮循环先判断 Map 中是否存在目标差值；
-     * 不存在则以当前元素为 Key、下标为 Value 存入 Map；
-     * 存在则从 Map 中以 目标差值为 Key 取出对应下标，与当前下标一并返回
-     *
      * Time: O(n), Space: O(n)
      *
      * @param nums
@@ -46,19 +41,24 @@ public class LeetCode1 {
      * @return
      */
     public int[] getTwoSumToTarget2(int[] nums, int target) {
+        // 缓存 Map，每轮循环先判断 Map 中是否存在目标差值。
         Map<Integer, Integer> cache = new HashMap<>(nums.length);
         for (int i = 0; i < nums.length; i++) {
             int num2 = target - nums[i];
+
+            // 存在则从 Map 中以 目标差值为 Key 取出对应下标，与当前下标一并返回。
             if (cache.containsKey(num2)) {
                 return new int[]{cache.get(num2), i};
             }
+            // 不存在则以当前元素为 Key、下标为 Value 存入 Map。
             cache.put(nums[i], i);
         }
         return new int[]{-1, -1};
     }
 
     /**
-     * 先排序，再用双指针法求解
+     * 先排序，再用双指针法求解。
+     * 这种解法会乱序，忽略。
      *
      * Time: O(nlog(n)), Space: O(1)
      *
