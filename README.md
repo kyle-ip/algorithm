5fc919b38587e82b2d7117d975f6bc60eed0c0b5
yipwinghong <yipwinghong@outlook.com>
2020-11-09 17:29:59 +0800

    :sparkles: [Java] LeetCode (973 medium)

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Data Structure &amp; Algorithm Problems' Solutions for interview, implemented in
 
 Mind Map: https://www.processon.com/view/link/5f9193b81e085307a09e598b
 
-## LeetCode (256)
+## LeetCode (257)
 
 | Tags          |                  |                     |                    |
 | ------------- | ---------------- | ------------------- | ------------------ |
@@ -278,6 +278,7 @@ Mind Map: https://www.processon.com/view/link/5f9193b81e085307a09e598b
 | 938  | [Range Sum of BST](https://leetcode.com/problems/range-sum-of-bst/submissions/) | 二叉搜索树的区间和 | Time: O(n)<br />Space: O(n) | [Java](java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode938.java) [Python](python/src/problem/leetcode/easy/leetcode_938.py) | Easy | Tree |
 | 945  | [ Minimum Increment to Make Array Unique](https://leetcode.com/problems/minimum-increment-to-make-array-unique) | 使数组唯一的最小增量               | Time: O(log(n))<br />Space: O(1) | [Java](java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode945.java) | Medium     | Array                                                  |
 | 946  | [Validate Stack Sequences](https://leetcode.com/problems/validate-stack-sequences) | 验证入栈出栈序列                   | Time: O(n)<br />Space: O(n) | [Java](java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode946.java) | Medium     | Stack                                                  |
+| 973  | [K Closest Points to Origin](https://leetcode-cn.com/problems/k-closest-points-to-origin/) | 最接近原点的 K 个点 | Time: O(n\*log(K))<br />Space: O(K) | [Java](java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode973.java) | Medium | Heap<br />Sorting<br />Divide and Conquer |
 | 1029 | [Two City Scheduling](https://leetcode.com/problems/two-city-scheduling) | 两地调度                           | Time: O(n\*log(n))<br />Space: O(n) | [Java](java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode1029.java) [Python](python/src/problem/leetcode/easy/leetcode_1029.py) | Easy       | Greedy                                                 |
 | 1304 | [Find N Unique Integers Sum up to Zero](https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero) | 和为零的 N 个唯一整数 | Time: O(n)<br />Space: O(1) | [Java](java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode1304.java) [Python](python/src/problem/leetcode/easy/leetcode_1304.py) | Easy | Array |
 
---
+++ java/src/main/java/com/ywh/problem/leetcode/medium/LeetCode973.java
@@ -0,0 +1,118 @@
+package com.ywh.problem.leetcode.medium;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+import static java.util.Comparator.comparingInt;
+
+/**
+ * 最接近原点的 K 个点
+ * [堆] [排序] [分治]
+ *
+ * @author ywh
+ * @since 2020/11/9/009
+ */
+public class LeetCode973 {
+
+    /**
+     * 大顶堆解法，求 Top K
+     *
+     * Time: O(n*log(K)), Space: O(K)
+     *
+     * @param points
+     * @param K
+     * @return
+     */
+    public int[][] kClosest(int[][] points, int K) {
+        // 大顶堆：按元组第一个值从大到小排列。
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((arr1, arr2) -> arr2[0] - arr1[0]);
+
+        // 先放入 K 个由距离和下标组成的元组。
+        for (int i = 0; i < K; i++) {
+            maxHeap.add(new int[]{points[i][0] * points[i][0] + points[i][1] * points[i][1], i});
+        }
+        // 把剩余的 points.length - K 个放入，如果距离比堆顶的距离小则替换。
+        for (int i = K; i < points.length; i++) {
+            int dist = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            if (dist < maxHeap.peek()[0]) {
+                maxHeap.poll();
+                maxHeap.add(new int[]{dist, i});
+            }
+        }
+        // 填充返回数据。
+        int[][] ret = new int[K][2];
+        for (int i = 0; i < K; i++) {
+            ret[i] = points[maxHeap.poll()[1]];
+        }
+        return ret;
+    }
+
+    /**
+     * 排序解法
+     *
+     * Time: O(n*log(n)), Space: O(log(n))
+     *
+     * @param points
+     * @param K
+     * @return
+     */
+    public int[][] kClosest2(int[][] points, int K) {
+        // 对距离按从小到大排列，返回前 K 个。
+        Arrays.sort(points, comparingInt(point -> (point[0] * point[0] + point[1] * point[1])));
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+
+    /**
+     * 快速选择解法（参考快排 {@link com.ywh.sorting.QuickSort #lomutoPartition}）
+     *
+     * @param points
+     * @param K
+     * @return
+     */
+    public int[][] kClosest3(int[][] points, int K) {
+        randomSelect(points, 0, points.length - 1, K);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    Random rand = new Random();
+
+    /**
+     * 快排中的划分操作每次执行完后都把数组分成两部分，左侧小于 pivot，右侧大于 pivot。
+     * 区别在于根据 K 与 pivot 下标位置关系，只处理划分结果的一部分。
+     *
+     * @param points
+     * @param left
+     * @param right
+     * @param K
+     */
+    public void randomSelect(int[][] points, int left, int right, int K) {
+        int pivotId = left + rand.nextInt(right - left + 1);
+        int pivot = points[pivotId][0] * points[pivotId][0] + points[pivotId][1] * points[pivotId][1];
+        swap(points, right, pivotId);
+        int i = left - 1;
+        for (int j = left; j < right; ++j) {
+            int dist = points[j][0] * points[j][0] + points[j][1] * points[j][1];
+            if (dist <= pivot) {
+                ++i;
+                swap(points, i, j);
+            }
+        }
+        ++i;
+        swap(points, i, right);
+        // [left, i-1] 都小于等于 pivot, [i+1, right] 都大于 pivot
+        if (K < i - left + 1) {
+            randomSelect(points, left, i - 1, K);
+        } else if (K > i - left + 1) {
+            randomSelect(points, i + 1, right, K - (i - left + 1));
+        }
+        // 如果 K == i - left + 1，表示 left 部分即为 K 个元素，结束整个过程。
+    }
+
+    public void swap(int[][] points, int index1, int index2) {
+        int[] temp = points[index1];
+        points[index1] = points[index2];
+        points[index2] = temp;
+    }
+}
