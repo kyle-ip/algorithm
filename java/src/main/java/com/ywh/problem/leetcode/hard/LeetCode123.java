737d2d2015e5e509f360482baaaf0818b9e13a92
yipwinghong <yipwinghong@outlook.com>
2020-12-21 17:46:45 +0800

    :sparkles: [Java] LeetCode (123 easy), (188 hard), (309 medium)

---
+++ java/src/main/java/com/ywh/problem/leetcode/hard/LeetCode123.java
@@ -0,0 +1,65 @@
+package com.ywh.problem.leetcode.hard;
+
+/**
+ * 买卖股票的最佳时机 III
+ * [数组] [动态规划]
+ *
+ * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+ * 注意: 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 示例 1:
+ *      输入: [3,3,5,0,0,3,1,4]
+ *      输出: 6
+ *      解释: 在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+ *           随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
+ * 示例 2:
+ *      输入: [1,2,3,4,5]
+ *      输出: 4
+ *      解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ *           注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ *           因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ * 示例 3:
+ *      输入: [7,6,4,3,1]
+ *      输出: 0
+ *      解释: 在这个情况下, 没有交易完成, 所以最大利润为 0。
+ *
+ * @author ywh
+ * @since 2020/12/21/021
+ */
+public class LeetCode123 {
+
+    /**
+     *
+     * @param nums
+     * @return
+     */
+    private int max(int ... nums) {
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    /**
+     * Time: O(n), Space: O(1)
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int n = prices.length;
+        //定义在 第 i 天时达到 4 种状态下的最大收益：第一次买入、第一次卖出、第二次买入、第二次卖出。
+        int dp1 = -prices[0], dp2 = 0, dp3 = -prices[0], dp4 = 0;
+        for (int i = 1; i < n; i++) {
+            dp1 = max(dp1, - prices[i]);
+            dp2 = max(dp2, dp1 + prices[i]);
+            dp3 = max(dp3, dp2 - prices[i]);
+            dp4 = max(dp4, dp3 + prices[i]);
+        }
+        return max(0, dp1, dp2, dp3, dp4);
+    }
+}
---
+++ java/src/main/java/com/ywh/problem/leetcode/hard/LeetCode188.java
@@ -0,0 +1,53 @@
+package com.ywh.problem.leetcode.hard;
+
+import java.util.Arrays;
+
+/**
+ * 买卖股票的最佳时机 IV
+ * [动态规划]
+ * 
+ * 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 示例 1：
+ *      输入：k = 2, prices = [2,4,1]
+ *      输出：2
+ *      解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
+ * 示例 2：
+ *      输入：k = 2, prices = [3,2,6,5,0,3]
+ *      输出：7
+ *      解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+ *           随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
+ * 提示：
+ *      0 <= k <= 10^9
+ *      0 <= prices.length <= 1000
+ *      0 <= prices[i] <= 1000
+ *
+ * @author ywh
+ * @since 2020/12/21/021
+ */
+public class LeetCode188 {
+
+    /**
+     * Time: O(n^2), Space: O(n)
+     *
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int k, int[] prices) {
+        // sell[i]、buy[i] 分别表示限制交易 i 次时在前一天卖出/买入得到的最大收益。
+        int[] sell = new int[k + 1], buy = new int[k + 1];
+        Arrays.fill(buy, -prices[0]);
+        // 遍历每天的价格。
+        for (int price : prices) {
+            // 第 i 次时买入，即从第 i-1 次卖出的最大收益减去当天价格。
+            // 第 i 次时卖出，即取“不卖出”和“上次买入所得最大收益加上今天价格”的较大者。
+            for (int i = 1; i < k + 1; i++) {
+                buy[i] = Math.max(buy[i], sell[i - 1] - price);
+                sell[i] = Math.max(sell[i], buy[i] + price);
+            }
+        }
+        return sell[k];
+    }
+}
