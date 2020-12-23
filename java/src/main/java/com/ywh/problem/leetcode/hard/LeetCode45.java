b89dd5b3cb8b5351ee03ae085cb7b4848a45ddbd
yipwinghong <yipwinghong@outlook.com>
2020-12-23 19:51:42 +0800

    :pencil: Add problem's description

--- java/src/main/java/com/ywh/problem/leetcode/hard/LeetCode45.java
+++ java/src/main/java/com/ywh/problem/leetcode/hard/LeetCode45.java
@@ -3,6 +3,17 @@ package com.ywh.problem.leetcode.hard;
 /**
  * 跳完数组的最少跳数
  * [数组] [贪心]
+ * 
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ * 示例:
+ *      输入: [2,3,1,1,4]
+ *      输出: 2
+ *      解释: 跳到最后一个位置的最小跳跃数是 2。
+ *            从下标为 0 跳到下标为 1 的位置，跳  1  步，然后跳  3  步到达数组的最后一个位置。
+ * 说明:
+ *      假设你总是可以到达数组的最后一个位置。
  *
  * @author ywh
  * @since 2019/11/29/029
@@ -75,5 +86,26 @@ public class LeetCode45 {
         }
 
         return -1;
+    }
+
+    /**
+     * Time: O(n), Space: O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public int jump(int[] nums) {
+        int length = nums.length, end = 0, maxPosition = 0, steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            // 每次找到可到达的最远位置（从 i 跳跃 nums[i] 位）。
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+
+            // 如果遍历走到该最远位置，则更新 end，并增加步数。
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
     }
 }
