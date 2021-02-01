51aed92f11ef854c8ead993484a7ddf607983b0d
yipwinghong <yipwinghong@outlook.com>
2021-02-01 17:56:19 +0800

    :pencil: Update comments

--- java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode110.java
+++ java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode110.java
@@ -3,8 +3,24 @@ package com.ywh.problem.leetcode.easy;
 import com.ywh.ds.tree.TreeNode;
 
 /**
- * 判断二叉树是否平衡
+ * 平衡二叉树
  * [树] [DFS]
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ * 示例 1：
+ *      输入：root = [3,9,20,null,null,15,7]
+ *      输出：true
+ * 示例 2：
+ *      输入：root = [1,2,2,3,3,null,null,4,4]
+ *      输出：false
+ * 示例 3：
+ *      输入：root = []
+ *      输出：true
+ * 提示：
+ *      树中的节点数在范围 [0, 5000] 内
+ *      -10^4 <= Node.val <= 10^4
  *
  * @author ywh
  * @since 2/18/2019
