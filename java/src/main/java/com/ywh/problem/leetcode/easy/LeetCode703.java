697a891631301a7a3033e58b995c8599a503bb05
yipwinghong <yipwinghong@outlook.com>
2021-02-11 11:21:03 +0800

    :pencil: Add comments

--- java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode703.java
+++ java/src/main/java/com/ywh/problem/leetcode/easy/LeetCode703.java
@@ -6,13 +6,38 @@ import java.util.Queue;
 /**
  * 数据流中第 K 大的元素
  * [堆]
+ * 
+ * 设计一个找到数据流中第 k 大元素的类（class）。注意是排序后的第 k 大元素，不是第 k 个不同的元素。
+ * 请实现 KthLargest 类：
+ *      KthLargest(int k, int[] nums) 使用整数 k 和整数流 nums 初始化对象。
+ *      int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 k 大的元素。
+ * 示例：
+ *      输入：
+ *          ["KthLargest", "add", "add", "add", "add", "add"]
+ *          [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+ *      输出：
+ *          [null, 4, 5, 5, 8, 8]
+ *      解释：
+ *          KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+ *          kthLargest.add(3);   // return 4
+ *          kthLargest.add(5);   // return 5
+ *          kthLargest.add(10);  // return 5
+ *          kthLargest.add(9);   // return 8
+ *          kthLargest.add(4);   // return 8
+ * 提示：
+ *      1 <= k <= 10^4
+ *      0 <= nums.length <= 10^4
+ *      -10^4 <= nums[i] <= 10^4
+ *      -10^4 <= val <= 10^4
+ *      最多调用 add 方法 10^4 次
+ *      题目数据保证，在查找第 k 大元素时，数组中至少有 k 个元素
  *
  * @author ywh
  * @since 2/22/2019
  */
 public class LeetCode703 {
 
-    public class KthLargestElementInStream {
+    public class KthLargest {
 
         /**
          * 优先级队列，默认即为最小堆，用于存放 Top K 元素
@@ -28,7 +53,7 @@ public class LeetCode703 {
          * @param k
          * @param nums
          */
-        public KthLargestElementInStream(int k, int[] nums) {
+        public KthLargest(int k, int[] nums) {
             this.k = k;
             for (int num: nums) {
                 add(num);
@@ -48,7 +73,7 @@ public class LeetCode703 {
             // 堆容量已满，与 minHeap 中的最小元素对比：
             // 如果新数字比较大，则把 minHeap 中的最小元素删除，加入新数字
             // 可确保当前堆中最小的元素为数据流中的第 k 大元素
-            else if(!minHeap.isEmpty() && val > minHeap.peek()) {
+            else if(val > minHeap.peek()) {
                 minHeap.poll();
                 minHeap.add(val);
             }
