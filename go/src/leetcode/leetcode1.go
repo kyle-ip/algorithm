421e3833054665cb1d0cd0065eee91ff04d5a616
yipwinghong <yipwinghong@outlook.com>
2021-02-18 18:32:37 +0800

    :art: replace tab with 4 spaces

--- go/src/leetcode/leetcode1.go
+++ go/src/leetcode/leetcode1.go
@@ -1,15 +1,14 @@
 package leetcode
 
-
 // Time: O(n), Space: O(n)
 func twoSum(nums []int, target int) []int {
-	m := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		num2 := target - nums[i]
-		if _, isExist := m[num2]; isExist {
-			return []int{m[num2], i}
-		}
-		m[nums[i]] = i
-	}
-	return nil
+    m := make(map[int]int)
+    for i := 0; i < len(nums); i++ {
+        num2 := target - nums[i]
+        if _, isExist := m[num2]; isExist {
+            return []int{m[num2], i}
+        }
+        m[nums[i]] = i
+    }
+    return nil
 }
--- go/src/leetcode/leetcode53.go
+++ go/src/leetcode/leetcode53.go
@@ -4,22 +4,22 @@ const MinInt = ^int(^uint(0) >> 1)
 
 // Time: O(1), Space: O(n)
 func maxSubArray(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	ret, cur := MinInt, 0
-	for _, num := range nums {
-		if cur >= 0 {
-			cur += num
-		} else {
-			cur = num
-		}
-		if cur > ret {
-			ret = cur
-		}
-	}
-	return ret
+    if len(nums) == 0 {
+        return 0
+    }
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    ret, cur := MinInt, 0
+    for _, num := range nums {
+        if cur >= 0 {
+            cur += num
+        } else {
+            cur = num
+        }
+        if cur > ret {
+            ret = cur
+        }
+    }
+    return ret
 }
